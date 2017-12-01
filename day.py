bf81af4d1ce335d112b299d118f24862be47c4c4
BraeWebb <email@braewebb.com>
2017-12-01 15:38:18 +1000

    Cleaned Day1 Solution

--- day.py
+++ day.py
@@ -1,7 +1,19 @@
 import sys
 
 def run(stdin):
-    pass
+    """
+    Takes problem input and yields solutions.
+
+    Parameters:
+        stdin (str): The input to the problem as a string.
+
+    Yields:
+        (*): The solutions to the problem.
+    """
+    yield
 
 if __name__ == "__main__":
-    run(sys.stdin.read())
+    if __name__ == "__main__":
+        results = run(sys.stdin.read())
+        for result in results:
+            print(result)
--- day1/day1.py
+++ day1/day1.py
@@ -1,6 +1,20 @@
 import sys
 
-def sum_matching(line, index_func=lambda x: x + 1):
+def sum_matching(line, index_func=lambda i: i + 1):
+    """
+    Computes the sum of matching pairs in a circular list.
+
+    A matching pair is when a number equals the number found at index_func(i)
+    in the list, where i is the numbers index and index_func is the provided
+    function. By default this is the next number, i.e. i + 1.
+
+    Parameters:
+        line (str): A string of numbers.
+        index_func (func): The function used to find a numbers match.
+
+    Returns:
+        (int): The sum of matching pairs.
+    """
     max = len(line)
     sum = 0
     for i, num in enumerate(line):
@@ -9,11 +23,22 @@ def sum_matching(line, index_func=lambda x: x + 1):
     return sum
 
 def run(stdin):
+    """
+    Takes problem input and yields solutions.
+
+    Parameters:
+        stdin (str): The input to the problem as a string.
+
+    Yields:
+        (*): The solutions to the problem.
+    """
     line = stdin.splitlines()[0]
     max = len(line)
 
-    print(sum_matching(line))
-    print(sum_matching(line, index_func=lambda i: i + (max // 2)))
+    yield sum_matching(line)
+    yield sum_matching(line, index_func=lambda i: i + (max // 2))
 
 if __name__ == "__main__":
-    run(sys.stdin.read())
+    results = run(sys.stdin.read())
+    for result in results:
+        print(result)
