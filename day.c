7df636e0b885a3df153d68a1abe492185b04fc6f
BraeWebb <email@braewebb.com>
2017-12-04 15:13:14 +1000

    Modified C Template

--- day.c
+++ day.c
@@ -5,11 +5,5 @@
 #include "../api.h"
 
 int main(int argc, char** argv) {
-	Input* input = load_input();
-
-	for (int i = 0; i < input->count; i++) {
-		printf("%s\n", input->lines[i]);
-	}
-
 	return 0;
 }
--- day2/day2.c
+++ day2/day2.c
@@ -1,5 +1,8 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
+
+#include "../api.h"
 
 int main(int argc, char** argv) {
     return 0;
