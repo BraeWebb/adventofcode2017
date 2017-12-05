1df3b790755685417613a68dee91e7db562f6167
BraeWebb <email@braewebb.com>
2017-12-05 15:32:31 +1000

    Completed Day5 in C

--- day5/day5.c
+++ day5/day5.c
@@ -4,6 +4,42 @@
 
 #include "../api.h"
 
+int jump(int* numbers, int numberCount, int (*replace)(int)) {
+    int current, last, count = 0;
+    while (current >= 0 && current < numberCount) {
+        last = current;
+        current += numbers[current];
+        numbers[last] = replace(numbers[last]);
+        count += 1;
+    }
+    return count;
+}
+
+int add_one(int oldValue) {
+	return oldValue + 1;
+}
+
+int stranger(int oldValue) {
+	if (oldValue >= 3) {
+		return oldValue - 1;
+	}
+	return oldValue + 1;
+}
+
 int main(int argc, char** argv) {
-	return 0;
+	Input* input = load_input();
+
+	int* partOne = malloc(sizeof(int) * input->count);
+	for (int i = 0; i < input->count; i++) {
+		partOne[i] = atoi(input->lines[i]);
+	}
+
+	printf("%d\n", jump(partOne, input->count, add_one));
+
+	int* partTwo = malloc(sizeof(int) * input->count);
+	for (int i = 0; i < input->count; i++) {
+		partTwo[i] = atoi(input->lines[i]);
+	}
+
+	printf("%d\n", jump(partTwo, input->count, stranger));
 }
