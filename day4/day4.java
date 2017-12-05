b521b9cdaa3ea8b4b9cfe135351e23bd3896d4a0
BraeWebb <email@braewebb.com>
2017-12-05 13:45:52 +1000

    Completed Day4 in Java

--- day4/day4.java
+++ day4/day4.java
@@ -1,7 +1,69 @@
-import java.util.Scanner;
+import java.io.BufferedInputStream;
+import java.util.*;
 
 public class day4 {
-    public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
+    private static boolean isValid(String phrase, Validator validator) {
+        String[] strings = phrase.split(" ");
+        String[] words = new String[strings.length];
+        int count = 0;
+        for (int i = 0; i < strings.length; i++) {
+            if (!validator.validate(strings[i], words)) {
+                return false;
+            }
+            words[count++] = strings[i];
+        }
+        return true;
     }
+
+    private static boolean isAnagram(String first, String second) {
+        char[] firsts = first.toCharArray();
+        if (second == null) {
+            return false;
+        }
+        char[] seconds = second.toCharArray();
+        Arrays.sort(firsts);
+        Arrays.sort(seconds);
+        return Arrays.equals(firsts, seconds);
+    }
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(new BufferedInputStream(System.in));
+
+        Validator isIn = (String word, String[] words) -> {
+            for (int i = 0; i < words.length; i++) {
+                if (word.equals(words[i])) {
+                    return false;
+                }
+            }
+            return true;
+        };
+
+        Validator isAnagram = (String word, String[] words) -> {
+            for (int i = 0; i < words.length; i++) {
+                if (isAnagram(word, words[i])) {
+                    return false;
+                }
+            }
+            return true;
+        };
+
+        int validOne = 0;
+        int validTwo = 0;
+        while (scan.hasNext()) {
+            String line = scan.nextLine();
+            if (isValid(line, isIn)) {
+                validOne += 1;
+            }
+            if (isValid(line, isAnagram)) {
+                validTwo += 1;
+            }
+        }
+
+        System.out.println(validOne);
+        System.out.println(validTwo);
+    }
+}
+
+interface Validator {
+    boolean validate(String word, String[] words);
 }
